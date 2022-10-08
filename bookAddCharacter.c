0b7e9ba8b02ab45267ef7425d285ff49aaf693de
Geoffrey Jarman <gjarman2020@gmail.com>
2022-10-08 13:18:17 -0500

    extras

--- bookAddCharacter.c
+++ bookAddCharacter.c
@@ -27,7 +27,7 @@
 // global declarations ------------------------------------------------------------------------------------------------
 
 char *sgServer = "192.168.0.13";                                                               //mysqlServer IP address
-char *sgUsername = "gjarman";                                                              // mysqlSerer logon username
+char *sgUsername = "gjarman";                                                             // mysqlServer logon username
 char *sgPassword = "Mpa4egu$";                                                    // password to connect to mysqlserver
 char *sgDatabase = "risingfast";                                                // default database name on mysqlserver
 
